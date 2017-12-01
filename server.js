67eedd1aa960785bb76862d9cfec4d08268f3ff8
Michael  Boyer <michaelboyer@Michaels-MacBook-Pro.local>
2017-12-01 15:14:30 -0700

    barely functioning

--- server.js
+++ server.js
@@ -5,26 +5,34 @@ var request     = require('request')
 const mongoose = require('mongoose')
 var app = express()
 
-mongoose.connect('mongodb://localhost:27017/travel-project', {useMongoClient: true})
+mongoose.connect('mongodb://localhost:27017/gymnastics', {useMongoClient: true})
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 
-app.use(express.static('./public'))
+app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/public'))
 
-var gymnasticsSchema = new mongoose.Schema({
+// var gymnasticsSchema = new mongoose.Schema({
 
-	name: {type: String},			//'Handstand to L-sit',
-	description: {type: String},	//'From handstand lower to L-sit or stradle L-sit',
-	value: {type: },				//this.floor.group1.A,
-	valuePoints: {type: },			//this.floor.group1.A.AValue,									
-	group:{type: String},			//'Group I',
-	groupDescription: {type: },		//this.floor.group1.description, 	
-	groupPoints: {type: },			//this.floor.group1.group1points,								
-	totalPoints: {type: Number},	//0,															
-	pic: {type: },
-	reference: {type: },
-	connection: {type: Boolean},	//true
+// 	name: {type: String},			//'Handstand to L-sit',
+// 	description: {type: String},	//'From handstand lower to L-sit or stradle L-sit',
+// 	value: {type: },				//this.floor.group1.A,
+// 	valuePoints: {type: },			//this.floor.group1.A.AValue,									
+// 	group:{type: String},			//'Group I',
+// 	groupDescription: {type: },		//this.floor.group1.description, 	
+// 	groupPoints: {type: },			//this.floor.group1.group1points,								
+// 	totalPoints: {type: Number},	//0,															
+// 	pic: {type: },
+// 	reference: {type: },
+// 	connection: {type: Boolean},	//true
+// })
+
+// var gymnasticsModel = mongoose.model('gymnastics', gymnasticsSchema)
+app.get('/', function(request, response){
+	response.sendFile(__dirname + '/gymnastics.html')
 })
 
-var gymnasticsModel = mongoose.model('gymnastics', gymnasticsSchema)
+app.listen(8080, function(){
+	console.log('the app is running on post 8080')
+})
